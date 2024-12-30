83f97335dc62b59d72605a7774d382981d227aca
alexl <alexl@DAD-PC>
2024-12-30 08:57:46 -0600

    Add project files.

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}
